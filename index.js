60e604028fd03e927c034ed1b1d40f82a170dba8
bregarulai <bregarulai@gmail.com>
2020-11-17 17:37:46 -0500

    save data to database using knex library

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const knex = require("knex");
 require("dotenv").config();
+
+const db = knex({
+  client: "pg",
+  version: "7.2",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DATABASE,
+  },
+});
 
 const app = express();
 app.use(express.json());
@@ -7,9 +19,25 @@ app.use(express.json());
 app.post("/submit", (req, res) => {
   const { firstName, lastName, email, date } = req.body;
   if (!firstName || !lastName || !email || !date) {
-    return res.status(400).json("Please fill all require fields");
+    return res.status(400).json("Please fill all required fields");
   }
-  res.send("Hello from api");
+  console.log(req.body);
+
+  db.returning("*")
+    .insert({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      date: date,
+    })
+    .into("event")
+    .then((event) => {
+      res.json({ body: event, message: "event saved successfully" });
+    })
+    .catch((err) => {
+      res.status(400).json("Unable to save event");
+      console.log(err);
+    });
 });
 
 app.listen(process.env.PORT || 3001, () => {
